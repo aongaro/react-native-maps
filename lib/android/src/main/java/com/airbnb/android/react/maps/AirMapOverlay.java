ff3e9382a02e6822d5d5c1bb7d2210105bd5f8e4
Andrea Ongaro <andrea.ongaro.kuro@gmail.com>
2017-05-24 15:29:02 +0200

    attempt using Fresco

--- lib/android/src/main/java/com/airbnb/android/react/maps/AirMapOverlay.java
+++ lib/android/src/main/java/com/airbnb/android/react/maps/AirMapOverlay.java
@@ -13,6 +13,18 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.GroundOverlay;
 import com.google.android.gms.maps.model.GroundOverlayOptions;
 
+import com.facebook.common.references.CloseableReference;
+import com.facebook.datasource.DataSource;
+import com.facebook.drawee.backends.pipeline.Fresco;
+import com.facebook.drawee.controller.BaseControllerListener;
+import com.facebook.drawee.controller.ControllerListener;
+import com.facebook.imagepipeline.core.ImagePipeline;
+import com.facebook.imagepipeline.image.CloseableImage;
+import com.facebook.imagepipeline.image.CloseableStaticBitmap;
+import com.facebook.imagepipeline.image.ImageInfo;
+import com.facebook.imagepipeline.request.ImageRequest;
+import com.facebook.imagepipeline.request.ImageRequestBuilder;
+
 
 import java.util.ArrayList;
 import java.util.List;
@@ -32,11 +44,44 @@ public class AirMapOverlay extends AirMapFeature {
     // private int fillColor;
     // private float strokeWidth;
     // private boolean geodesic;
-    private BitmapDescriptor image;
+    private BitmapDescriptor imgBitmapDescriptor;
     private boolean visible;
     private float transparency;
     private float zIndex;
     private float markerHue = 0.0f;
+
+    private Bitmap imgBitmap;
+    private DataSource<CloseableReference<CloseableImage>> dataSource;
+    private final ControllerListener<ImageInfo> mLogoControllerListener =
+            new BaseControllerListener<ImageInfo>() {
+                @Override
+                public void onFinalImageSet(
+                        String id,
+                        @Nullable final ImageInfo imageInfo,
+                        @Nullable Animatable animatable) {
+                    CloseableReference<CloseableImage> imageReference = null;
+                    try {
+                        imageReference = dataSource.getResult();
+                        if (imageReference != null) {
+                            CloseableImage image = imageReference.get();
+                            if (image != null && image instanceof CloseableStaticBitmap) {
+                                CloseableStaticBitmap closeableStaticBitmap = (CloseableStaticBitmap) image;
+                                Bitmap bitmap = closeableStaticBitmap.getUnderlyingBitmap();
+                                if (bitmap != null) {
+                                    // bitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true);
+                                    imgBitmap = bitmap;
+                                    imgBitmapDescriptor = BitmapDescriptorFactory.fromBitmap(bitmap);
+                                }
+                            }
+                        }
+                    } finally {
+                        dataSource.close();
+                        if (imageReference != null) {
+                            CloseableReference.closeSafely(imageReference);
+                        }
+                    }
+                }
+            };
 
     public AirMapOverlay(Context context) {
         super(context);
@@ -68,16 +113,21 @@ public class AirMapOverlay extends AirMapFeature {
 
     public void setImage(String uri) {
         if (uri == null) {
-            this.image = null;
+            imgBitmapDescriptor = null;
         } else if (uri.startsWith("http://") || uri.startsWith("https://") ||
                 uri.startsWith("file://")) {
             try {
-                URL url = new URL(uri); 
-                Bitmap bmp = BitmapFactory.decodeStream(url.openConnection().getInputStream()); 
-                this.image = BitmapDescriptorFactory.fromBitmap(bmp);
-                if (overlay != null) {
-                    overlay.setImage(image);
-                }
+                ImageRequest imageRequest = ImageRequestBuilder
+                    .newBuilderWithSource(Uri.parse(uri))
+                    .build();
+                ImagePipeline imagePipeline = Fresco.getImagePipeline();
+                dataSource = imagePipeline.fetchDecodedImage(imageRequest, this);
+                // URL url = new URL(uri); 
+                // Bitmap bmp = BitmapFactory.decodeStream(url.openConnection().getInputStream()); 
+                // this.image = BitmapDescriptorFactory.fromBitmap(bmp);
+                // if (overlay != null) {
+                //     overlay.setImage(image);
+                // }
             } catch (MalformedURLException e) {
                 System.err.println("MalformedURLException: " + e.getMessage());
             } catch (IOException e) {
